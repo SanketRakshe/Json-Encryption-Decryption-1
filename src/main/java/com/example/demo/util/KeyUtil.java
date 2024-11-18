09284fcbe9883d92e0f02e7a6073182f5b6a7d87
SanketRakshe <sanketrakshe07@gmail.com>
2024-11-18 21:40:59 +0530

    Reafactored..!

--- src/main/java/com/example/demo/util/KeyUtil.java
+++ src/main/java/com/example/demo/util/KeyUtil.java
@@ -3,6 +3,7 @@ package com.example.demo.util;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class KeyUtil {
  private static KeyPair keyPair;
@@ -12,6 +13,14 @@ public class KeyUtil {
          KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
          keyPairGenerator.initialize(2048);
          keyPair = keyPairGenerator.generateKeyPair();
+         
+         String publicKey = Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded()); 
+         String privateKey = Base64.getEncoder().encodeToString(keyPair.getPrivate().getEncoded()); 
+         
+         System.out.println("Public Key: " + publicKey); 
+         
+         System.out.println("Private Key: " + privateKey);
+         
      } catch (NoSuchAlgorithmException e) {
          e.printStackTrace();
      }
