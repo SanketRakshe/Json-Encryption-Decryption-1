c9c9ba885d4a8fc7fe3b6d7e151e2b9be4956d53
SanketRakshe <sanketrakshe07@gmail.com>
2024-11-18 11:43:55 +0530

    Initial commit: Project setup done..!

---
+++ src/main/java/com/example/demo/EncryptionDecryption1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionDecryption1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionDecryption1Application.class, args);
+		System.out.println("started..!");
+	}
+
+}
